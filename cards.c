69226ffaa2cd29c94f9cc56f82058f6879f266c0
Kentaro Kawano <kentaro.kawano@gmail.com>
2014-01-06 12:50:22 +0900

    練習(p.35)

--- cards.c
+++ cards.c
@@ -6,26 +6,38 @@
 
 int main() {
   char card_name[3];
-  puts("カード名を入力してください:");
-  scanf("%2s", card_name);
-  int val = 0;
-  switch (card_name[0]) {
-    case 'K':
-    case 'Q':
-    case 'J':
-      val = 10;
-      break;
-    case 'A':
-      val = 11;
-      break;
-    default:
-      val = atoi(card_name);
-  }
-  // 値が 3 から 6 かどうかを調べる
-  if ((val > 2) && (val < 7))
-    puts("カウントが増えます");
-  // それ以外の場合には、カードが 10,J,Q,K かどうか調べる
-  else if (val == 10)
-    puts("カウントが減ります");
+  int count = 0;
+
+  do {
+    puts("カード名を入力してください:");
+    scanf("%2s", card_name);
+    int val = 0;
+    switch (card_name[0]) {
+      case 'K':
+      case 'Q':
+      case 'J':
+        val = 10;
+        break;
+      case 'A':
+        val = 11;
+        break;
+      case 'X':
+        continue;
+      default:
+        val = atoi(card_name);
+        if ((val < 1) || (val > 10)) {
+          puts("無効なカードです");
+          continue;
+        }
+    }
+
+    if ((val > 2) && (val < 7)) {
+      count++;
+    } else if (val == 10) {
+      count--;
+    }
+    printf("現在のカウント: %i\n", count);
+
+  } while (card_name[0] != 'X');
   return 0;
 }
