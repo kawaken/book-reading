13fd48169492f069bb93f0887811a8d20c71d499
Kentaro Kawano <kentaro.kawano@gmail.com>
2014-01-15 12:34:33 +0900

    練習(p.95)

--- tracks.c
+++ tracks.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+
+char tracks[][80] = {
+  "I left my heart in Harvard Med School",
+  "Newark, Newark - a wonderful town",
+  "Dancing with a Dork",
+  "From here to maternity",
+  "The girl from Iwo Jima",
+};
+
+void find_track(char search_for[]) {
+  int i;
+  for (i = 0; i < 5; i++) {
+    if (strstr(tracks[i], search_for)) {
+      printf("曲番頭%i: '%s'\n", i, tracks[i]);
+    }
+  }
+}
 
 int main() {
-  char tracks[][80] = {
-    "I left my heart in Harvard Med School",
-    "Newark, Newark - a wonderful town",
-    "Dancing with a Dork",
-    "From here to maternity",
-    "The girl from Iwo Jima",
-  };
 
-  printf("%s\n", tracks[4]);
-  printf("%c\n", tracks[4][6]);
+  char search_for[80];
+  printf("検索語:");
+  fgets(search_for, 80, stdin);
+  search_for[strlen(search_for) - 1] = '\0';
+  find_track(search_for);
 
   return 0;
 }
