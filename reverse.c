52f4986f7ea87bf967591c0a9a494749a2af34af
Kentaro Kawano <kentaro.kawano@gmail.com>
2014-01-15 12:56:46 +0900

    練習(p.100)

--- reverse.c
+++ reverse.c
@@ -12,5 +12,25 @@ void print_reverse(char *s) {
 }
 
 int main() {
-  print_reverse("hogehoge");
+  char *juices[] = {
+    "dragonfruit", "waterberry", "sharonfruit", "uglifruit",
+    "rumberry", "kiwifruit", "mulberry", "strawberry",
+    "blueberry", "blackberry", "starfruit"
+  };
+  char *a;
+  
+  puts(juices[6]);
+  print_reverse(juices[7]);
+  a = juices[2];
+  juices[2] = juices[8];
+  juices[8] = a;
+  puts(juices[8]);
+  print_reverse(juices[(18 + 7) / 5]);
+  puts(juices[2]);
+  print_reverse(juices[9]);
+  juices[1] = juices[3];
+  puts(juices[10]);
+  print_reverse(juices[1]);
+
+  return 0;
 }
