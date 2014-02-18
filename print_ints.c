2805e5771181987e90edf615f7e19f7388f1cf27
Kentaro Kawano <kentaro.kawano@gmail.com>
2014-02-18 12:50:57 +0900

    練習(p.346)

---
+++ print_ints.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void print_ints(int args, ...) {
+  va_list ap;
+  va_start(ap, args);
+  int i;
+  for (i = 0; i < args; i++) {
+    printf("引数:%i\n", va_arg(ap, int));
+  }
+  va_end(ap);
+}
+
+int main() {
+  print_ints(3, 79, 101, 32);
+}
