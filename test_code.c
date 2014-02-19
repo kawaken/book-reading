9beb3c91322e84f34104e940b911670814e7e7a0
Kentaro Kawano <kentaro.kawano@gmail.com>
2014-02-19 12:50:43 +0900

    練習(p.355)

---
+++ test_code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "encrypt.h"
+#include "checksum.h"
+
+int main() {
+  char s[] = "Speak friend and enter";
+  encrypt(s);
+  printf("'%s'に暗号化しました\n", s);
+  printf("チェックサムは%iです\n", checksum(s));
+
+  encrypt(s);
+  printf("'%s'に複合化しました\n", s);
+  printf("チェックサムは%iです\n", checksum(s));
+
+  return 0;
+}
